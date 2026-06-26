c513911ebfcf1f795fcc291c4e91eeea4bdd5e51
Pavlo Kovalenko <pashko.dev@gmail.com>
2026-06-26 17:04:07 +0300

    blog section completed

--- src/components/layout/slider/slider.js
+++ src/components/layout/slider/slider.js
@@ -1,111 +1,155 @@
 import Swiper from 'swiper'
-import { Navigation, EffectFade, Thumbs } from 'swiper/modules'
+import { Navigation, EffectFade, Thumbs, Pagination } from 'swiper/modules'
 
 import './slider.scss'
-// import 'swiper/css/effect-fade'
 
-export async function initHowWorksSliders() {
+export async function initSliders() {
+	/**
+	 * =========================
+	 * HOW WORKS SLIDERS
+	 * =========================
+	 */
 	const thumbsEl = document.querySelector('.how-works__thumbs-slider')
 	const mediaEl = document.querySelector('.how-works__media-slider')
 	const contentEl = document.querySelector('.how-works__content-slider')
 	const navEl = document.querySelector('.how-works__nav-slider')
 
-	if (!thumbsEl || !mediaEl || !contentEl || !navEl) return
+	if (thumbsEl && mediaEl && contentEl && navEl) {
+		const thumbsSlider = new Swiper(thumbsEl, {
+			modules: [Navigation],
 
-	// THUMBS
-	const thumbsSlider = new Swiper(thumbsEl, {
-		modules: [Navigation],
+			observer: true,
+			observeParents: true,
 
-		observer: true,
-		observeParents: true,
+			direction: 'vertical',
+			slidesPerView: 4,
+			spaceBetween: 12,
 
-		direction: 'vertical',
+			speed: 800,
 
-		slidesPerView: 4,
-		spaceBetween: 12,
+			watchSlidesProgress: true,
+			slideToClickedSlide: true,
+		})
 
-		speed: 800,
+		const navSlider = new Swiper(navEl, {
+			modules: [EffectFade],
 
-		watchSlidesProgress: true,
-		slideToClickedSlide: true,
-	})
+			observer: true,
+			observeParents: true,
 
-	// NAV TITLE
-	const navSlider = new Swiper(navEl, {
-		modules: [EffectFade],
+			slidesPerView: 1,
+			speed: 800,
+			allowTouchMove: false,
 
-		observer: true,
-		observeParents: true,
+			effect: 'fade',
+			fadeEffect: {
+				crossFade: true,
+			},
+		})
 
-		slidesPerView: 1,
+		const contentSlider = new Swiper(contentEl, {
+			modules: [EffectFade],
 
-		speed: 800,
+			observer: true,
+			observeParents: true,
 
-		allowTouchMove: false,
+			slidesPerView: 1,
+			speed: 800,
+			allowTouchMove: false,
 
-		effect: 'fade',
-		fadeEffect: {
-			crossFade: true,
-		},
-	})
+			effect: 'fade',
+			fadeEffect: {
+				crossFade: true,
+			},
+		})
 
-	// CONTENT
-	const contentSlider = new Swiper(contentEl, {
-		modules: [EffectFade],
+		const mediaSlider = new Swiper(mediaEl, {
+			modules: [Navigation, EffectFade, Thumbs],
 
-		observer: true,
-		observeParents: true,
+			observer: true,
+			observeParents: true,
 
-		slidesPerView: 1,
+			slidesPerView: 1,
+			speed: 800,
 
-		speed: 800,
+			effect: 'fade',
+			fadeEffect: {
+				crossFade: true,
+			},
 
-		allowTouchMove: false,
+			thumbs: {
+				swiper: thumbsSlider,
+			},
 
-		effect: 'fade',
-		fadeEffect: {
-			crossFade: true,
-		},
-	})
+			navigation: {
+				prevEl: '.how-works__swiper-button-prev',
+				nextEl: '.how-works__swiper-button-next',
+			},
+		})
 
-	// MEDIA (главный)
-	const mediaSlider = new Swiper(mediaEl, {
-		modules: [Navigation, EffectFade, Thumbs],
+		const sync = (index) => {
+			contentSlider.slideTo(index)
+			navSlider.slideTo(index)
+		}
 
-		observer: true,
-		observeParents: true,
+		mediaSlider.on('slideChange', () => {
+			sync(mediaSlider.activeIndex)
+		})
 
-		slidesPerView: 1,
+		thumbsSlider.on('slideChange', () => {
+			sync(thumbsSlider.activeIndex)
+		})
+	}
 
-		speed: 800,
+	/**
+	 * =========================
+	 * TRUST SLIDER
+	 * =========================
+	 */
+	const trustEl = document.querySelector('.trust__slider')
 
-		effect: 'fade',
-		fadeEffect: {
-			crossFade: true,
-		},
+	if (trustEl) {
+		new Swiper(trustEl, {
+			modules: [Navigation, Pagination],
 
-		thumbs: {
-			swiper: thumbsSlider,
-		},
+			observer: true,
+			observeParents: true,
+			autoHeight: true,
+			slidesPerView: 2,
+			spaceBetween: 20,
+			speed: 700,
 
-		navigation: {
-			prevEl: '.how-works__swiper-button-prev',
-			nextEl: '.how-works__swiper-button-next',
-		},
-	})
+			navigation: {
+				prevEl: '.trust__swiper-button-prev',
+				nextEl: '.trust__swiper-button-next',
+			},
 
-	// синхронизация всех остальных
-	mediaSlider.on('slideChange', () => {
-		const index = mediaSlider.activeIndex
+			pagination: {
+				el: '.trust__swiper-pagination',
+				type: 'fraction',
+				renderFraction: (currentClass, totalClass) => {
+					return `
+						<span class="${currentClass}"></span>
+						<span class="trust__pagination-separator"> / </span>
+						<span class="${totalClass}"></span>
+					`
+				},
+			},
 
-		contentSlider.slideTo(index)
-		navSlider.slideTo(index)
-	})
-
-	thumbsSlider.on('slideChange', () => {
-		const index = thumbsSlider.activeIndex
-
-		contentSlider.slideTo(index)
-		navSlider.slideTo(index)
-	})
+			breakpoints: {
+				320: {
+					slidesPerView: 1.15,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 2.2,
+					spaceBetween: 20,
+				},
+				1480: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+			},
+		})
+	}
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,14 @@
 import { addTouchAttr, addLoadedAttr } from '@js/common/functions.js'
 import { initLenis } from './custom/lenis.js'
 import { initScrollStory, initHeroIntro } from './custom/gsap-animations.js'
-import { initHowWorksSliders } from '../components/layout/slider/slider.js'
+import { initSliders } from '../components/layout/slider/slider.js'
 
 addTouchAttr()
 
 initLenis()
 
 addLoadedAttr(async () => {
-	await initHowWorksSliders()
+	await initSliders()
 
 	initScrollStory()
 
